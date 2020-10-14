db8d3986e07c8e0764e8f80b11491203e10fa462
Nicolas Bertrand <bertrand.nicolas.90@gmail.com>
2020-10-14 15:53:04 +0200

    Cleanup

--- xmonad.hs
+++ xmonad.hs
@@ -1,40 +1,29 @@
--- Imports.
+-- XMobase base
 import XMonad
 import XMonad.Hooks.DynamicLog
 import qualified XMonad.StackSet as W
-import XMonad.Util.EZConfig
 
+-- XMonad utils
+import XMonad.Util.EZConfig (additionalKeysP, checkKeymap)
+
+-- Utils
 import Control.Monad.IO.Class (liftIO)
 import System.Posix.Env (putEnv)
 
--- The main function.
-main = xmonad =<< statusBar myBar myPP toggleBarKeyBiding myConfig
-
--- Command to launch the bar.
-myBar = "xmobar"
-
--- Bar's pretting printing configuration,
-myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
-
--- Key binding to toggle the bar.
-toggleBarKeyBiding XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
-
--- Main configuration.
-myConfig = def
-  { modMask = mod4Mask
-  , terminal = "urxvt" -- TODO add urxvtcd
-  , workspaces = myWorkspaces
-  , startupHook = myStartupHook
-  }
-  `additionalKeysP`
-  (myKeyBindings myConfig)
-
+-- Variables
 myWorkspaces = ["term", "firefox", "chrome", "emacs", "dev", "misc"]
 
-myEditor =  "emacsclient --alternate-editor=\"\" -c \"$@\""
+myModMask = mod4Mask
+
+myEditor = "emacsclient --alternate-editor=\"\" -c \"$@\""
+
 myWebBrowser = "icecat"
+
 myLauncher = "rofi -show drun -theme slate -icon-theme Papirus -show-icons"
+
 myScreenshot = "scrot -q 90"
+
+myTerminal = "urxvt" -- TODO change with urxvtcd
 
 -- Key configuration
 myKeyBindings cfg =
@@ -58,10 +47,33 @@ myKeyBindings cfg =
 myStartupHook = do
   return ()
   mapM_ (liftIO . putEnv)
-    [ "_JAVA_AWT_WM_NONREPARENTING=1" ]
+    [ "_JAVA_AWT_WM_NONREPARENTING=1"
+    , "EDITOR=" ++ myEditor]
   mapM_ spawn
     [ "nitrogen --restore"             -- load wallpaper
     , "xrdb ~/.Xresources"             -- load config
     , "xsetroot -cursor_name left_ptr" -- load mouse cursor
     ]
   checkKeymap myConfig (myKeyBindings myConfig)
+
+-- Main configuration.
+myConfig = def
+  { modMask = myModMask
+  , terminal = myTerminal
+  , workspaces = myWorkspaces
+  , startupHook = myStartupHook
+  }
+  `additionalKeysP`
+  (myKeyBindings myConfig)
+
+-- The main function.
+main = xmonad =<< statusBar myBar myPP toggleBarKeyBiding myConfig
+
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Bar's pretting printing configuration,
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- Key binding to toggle the bar.
+toggleBarKeyBiding XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
